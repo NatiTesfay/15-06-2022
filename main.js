6d743ba378c0d69e099c6de695ddc91fa53f0a23
NatiTesfay <netaneltes66@gmail.com>
2022-06-15 22:38:34 +0300

    Array-15.06.2022

---
+++ main.js
@@ -0,0 +1,32 @@
+function getWords() {
+    let name = ['avi','asi','natan','chen'];
+
+   name.sort()
+   console.log(name);
+    
+}
+getWords();
+
+function getNumbers() {
+    let num = [1,2,8,10,15];
+    num.sort((a,b)=>{ return a-b})
+    console.log(num);
+    
+}
+getNumbers();
+
+function getNumbersFromBtS() {
+    let num = [1,2,8,10,15];
+    num.sort((a,b)=>{ return b-a})
+    console.log(num);
+    
+}
+getNumbersFromBtS();
+function randomNumbers() {
+    for (let i = 0; i < 10 ; i++) {
+     console.log(NumArray.value)
+        
+        
+    }
+    
+}
