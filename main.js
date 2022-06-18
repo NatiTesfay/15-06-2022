189de22f7e85c4141a9602ac3252090da79e7303
NatiTesfay <netaneltes66@gmail.com>
2022-06-18 23:08:53 +0300

    array-18.08.2022

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-function getWords() {
-    let name = ['avi','asi','natan','chen'];
+// function getWords() {
+//     let name = ['avi','asi','natan','chen'];
 
-   name.sort()
-   console.log(name);
+//    name.sort()
+//    console.log(name);
     
-}
-getWords();
+// }
+// getWords();
 
-function getNumbers() {
-    let num = [1,2,8,10,15];
-    num.sort((a,b)=>{ return a-b})
-    console.log(num);
+// function getNumbers() {
+//     let num = [1,2,8,10,15];
+//     num.sort((a,b)=>{ return a-b})
+//     console.log(num);
     
-}
-getNumbers();
+// }
+// getNumbers();
 
-function getNumbersFromBtS() {
-    let num = [1,2,8,10,15];
-    num.sort((a,b)=>{ return b-a})
-    console.log(num);
+// function getNumbersFromBtS() {
+//     let num = [1,2,8,10,15];
+//     num.sort((a,b)=>{ return b-a})
+//     console.log(num);
     
-}
-getNumbersFromBtS();
-function randomNumbers() {
-    for (let i = 0; i < 10 ; i++) {
-     console.log(NumArray.value)
-        
-        
-    }
+// }
+
+let userValue = [];
+let nameFromUser = document.getElementById('NumArray');
+function BTN() {
+    userValue.push(nameFromUser.value);
+    userValue.sort((a,b)=>{return a - b})
+    userValue.join(",")
+    console.log(userValue)
     
-}
+  } 
+
